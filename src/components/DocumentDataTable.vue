1233ca8c0684000afe5d6dff65f8610cdf6f8cdc
Christian <christian.cruvinel.franca@gmail.com>
2021-06-16 18:19:17 -0300

    csv

--- src/components/DocumentDataTable.vue
+++ src/components/DocumentDataTable.vue
@@ -233,7 +233,7 @@ export default {
             const url = window.URL.createObjectURL(new Blob([response.data]))
             const link = document.createElement('a')
             link.href = url
-            link.setAttribute('download', `${this.$store.getters.getCurrentCollectionName}.xlsx`)
+            link.setAttribute('download', `${this.$store.getters.getCurrentCollectionName}.csv`)
             document.body.appendChild(link)
             link.click()
         },
